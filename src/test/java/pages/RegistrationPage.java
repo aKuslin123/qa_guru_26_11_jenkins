6697e5d6b38d78bb7d6e0c18a521a0f69f2351fc
aKuslin123 <a.Kuslin123@gmail.com>
2024-05-10 17:21:39 +0700

    добавил алюр степы

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.FormComponent;
 import pages.components.ValidationComponent;
@@ -30,6 +31,7 @@ public class RegistrationPage {
     FormComponent formComponent = new FormComponent();
     ValidationComponent validationComponent = new ValidationComponent();
 
+    @Step("Открываем страницу регистрационной формы")
     public RegistrationPage openPage(){
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
@@ -39,36 +41,42 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Вводим имя {value}")
     public RegistrationPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Вводим фамилию {value}")
     public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Вводим емаил {value}")
     public RegistrationPage setUserEmail(String value) {
         userEmailInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем пол {value}")
     public RegistrationPage setGender(String value) {
         genderWrapper.$(byText(value)).click();
 
         return this;
     }
 
+    @Step("Вводим номер телефона {value}")
     public RegistrationPage setUserNumber(String value) {
         userNumberInput.setValue(value);
 
         return this;
     }
 
+    @Step("Указываем дату рождения {day}/{month}/{year}")
     public RegistrationPage setBirthDate(String day, String month, String year) {
         calendarInput.click();
         calendarComponent.setDate(day, month, year);
@@ -76,30 +84,35 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Выбираем предмет {value}")
     public RegistrationPage setSubject(String value) {
         subjectsInput.setValue(value).pressEnter();
 
         return this;
     }
 
+    @Step("Выбираем хобби {value}")
     public RegistrationPage setHobby(String value) {
         hobbiesWrapper.$(byText(value)).click();
 
         return this;
     }
 
+    @Step("Загружаем картинку {value}")
     public RegistrationPage uploadPicture(String value) {
         uploadPicture.uploadFromClasspath(value);
 
         return this;
     }
 
+    @Step("Вводим адресс {value}")
     public RegistrationPage setAddress(String value) {
         addressInput.setValue(value);
 
         return this;
     }
 
+    @Step("Выбираем штат {value}")
     public RegistrationPage setState(String value) {
         stateInput.click();
         stateCityWrapper.$(byText(value)).click();
@@ -107,6 +120,7 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Выбираем город {value}")
     public RegistrationPage setCity(String value) {
         cityInput.click();
         stateCityWrapper.$(byText(value)).click();
@@ -114,30 +128,35 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Жмем кнопку submit")
     public RegistrationPage pressSubmit() {
         submitButton.click();
 
         return this;
     }
 
+    @Step("Проверяем, что введенное значение {value} совпадает с таблицей {key}")
     public RegistrationPage checkResult(String key, String value) {
         formComponent.checkForm(key, value);
 
         return this;
     }
 
+    @Step("Проверяем, что таблица с результатами заполнения не появилась")
     public RegistrationPage checkFormNotDisplayed() {
         formComponent.formDoesNotAppear();
 
         return this;
     }
 
+    @Step("Валидация пола")
     public RegistrationPage checkGenderValidation(String value) {
         validationComponent.genderValidation(value);
 
         return this;
     }
 
+    @Step("Валидация обязательных полей инпутов")
     public RegistrationPage checkInputValidation(String value) {
         validationComponent.inputValidation(value);
 
